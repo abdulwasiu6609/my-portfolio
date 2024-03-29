765160bf41226198f5e57c0f92c524658029f771
ABDUL WASIU ADEBAYO <99112149+abdulwasiu6609@users.noreply.github.com>
2024-03-29 12:38:29 +0000

    another test

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -58,13 +58,15 @@ export default function About() {
               liveProject={project.liveProject}
             />
           ))}
+
+          <Link
+            className="text-white w-[100%] sm:w-fit  text-center bg-blue-light px-4 py-2 rounded-lg hover:bg-blue-dark mt-6 -mb-4 "
+            href="/projects"
+          >
+            View all projects
+          </Link>
         </div>
-        <Link
-          className="text-white w-[100%] sm:w-fit  text-center bg-blue-light px-4 py-2 rounded-lg hover:bg-blue-dark mt-6 -mb-4 "
-          href="/projects"
-        >
-          View all projects
-        </Link>
+
       </section>
       <CTASection className="text-white" />
     </div>
